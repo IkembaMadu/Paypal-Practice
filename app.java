3e769fd9e589b4c94ed755daebc558afca871e84
IkembaMadu <ikembamadu@gmail.com>
2023-03-04 19:00:15 -0500

    modified

--- app.java
+++ app.java
@@ -5,3 +5,10 @@ NodeJS applications,
 python applications,
 web applications,
 bug removed
+This is great
+developing java apps
+NodeJS applications
+Great
+This app is excellent
+This app is excellent for all users
+>>>>>>> e0a29e8 (excellent app for all)
